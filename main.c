29b9e44368667e9acfa3ee8bb0efbf10d23f1006
AniKrisn <anirudh.krishnan@hotmail.com>
2024-08-10 13:25:27 +0530

    start lval, before changing eval

--- main.c
+++ main.c
@@ -64,6 +64,54 @@ long eval(mpc_ast_t *t) {
 }
 
 
+// def "lisp value" -- 
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+
+// possible lval types
+enum { LVAL_NUM, LVAL_ERR };
+
+// possible err types
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// number type lval
+lval lval_num (long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+// err type lval
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+// print an lval
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li", v.num); break;
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) {
+                printf("Error: Division by Zero!");
+            }
+            if (v.err == LERR_BAD_NUM) {
+                printf("Error: Invalid Number!");
+            }
+        break;
+    }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+
 int main (int argc, char **argv) {
 
     mpc_parser_t *Number = mpc_new("number");
