890bfaddc2609c5b65966047d3d1647cde290f41
AniKrisn <anirudh.krishnan@hotmail.com>
2024-08-17 12:11:16 +0530

    add lval_read, lval_add

--- main.c
+++ main.c
@@ -80,7 +80,7 @@ lval *lval_sexpr(void) {
     return v;
 }
 
-// 
+// destructor for lval, frees the memory used by the lval
 void lval_del(lval *v) {
     switch (v->type) {
         // for nums the type is long so nothing special
@@ -100,6 +100,38 @@ void lval_del(lval *v) {
     free(v);
 }
 
+lval lval_read_num(mpc_ast_t *t) {
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+}
+
+lval *lval_read(mpc_ast_t *t) {
+    // if the tag is a number or sym return a pointer to a num lval
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    lval *x = NULL;
+    if (strcmp(t->tag, ">") == 0) {x = lval_sexpr();}
+    if (strcmp(t->tag, "sexpr")) { x = lval_sexpr();}
+
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
+}
+
+
+lval *lval_add(lval *v, lval *x) {
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    v->cell[v->count-1] = x;
+    return v;
+}
 
 
 
